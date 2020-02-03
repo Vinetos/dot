890f1227d7931865a8f4c666696b0bfc5d253be5
Valentin Chassignol <valentin.chassignol@epita.fr>
2020-02-03 21:13:27 +0100

    feat: First commit

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/vinetos/.local/share/omf/themes/bobthefish/fish_prompt.fish
---
+++ .config/ui-update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wal -i ~/Pictures/390985876.jpg
+
+~/.config/polybar/polybar.sh &
+~/.config/dunst/dunst.sh
+~/.config/firefoxChrome.sh
